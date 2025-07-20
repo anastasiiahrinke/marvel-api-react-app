eb10a879bf908122970ea64232f577f4ef0de3e5
Anastasiia <anasrasia,grinke2005@gmail.com>
2025-07-21 00:09:41 +0300

    Add Marvel API React app with character components and fixed API integration

---
+++ src/services/MarvelService.js
@@ -0,0 +1,41 @@
+class MarvelService {
+    _apiBase = 'https://marvel-server-zeta.vercel.app/';
+    _apiKey = 'd4eecb0c66dedbfae4eab45d312fc1df';
+
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=10&apikey=${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const url = `${this._apiBase}characters/${id}?apikey=${this._apiKey}`;
+        const res = await this.getResource(url);
+        if (!res.data.results || res.data.results.length === 0) {
+            throw new Error(`Character with ID ${id} not found`);
+        }
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
